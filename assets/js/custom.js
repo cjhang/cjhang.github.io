e8a75cf1e484d1df3db262496e3d30a3f3455525
Jianhang Chen <cjhastro@gmail.com>
2023-01-14 19:03:07 +0100

    change to dark theme and add background image

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,22 @@
+// include HTML snippet
+function includeHTML() {
+    for (var t, e, n, s = document.getElementsByTagName("*"), i = 0; i < s.length; i++)
+        if (e = (t = s[i]).getAttribute("html-snippet")) return (n = new XMLHttpRequest).onreadystatechange = function() {
+            4 == this.readyState && (200 == this.status && (t.innerHTML = this.responseText), 404 == this.status && (t.innerHTML = "Page not found."), t.removeAttribute("html-snippet"), includeHTML())
+        }, n.open("GET", e, !0), void n.send()
+};
+
 // make interactive navbar
-
 function toggleBurger(event) {
-burger = document.getElementsByClassName('burger')[0];
-burgernav = document.querySelector('#'+burger.dataset.target);
+  burger = document.getElementsByClassName('burger')[0];
+  burgernav = document.querySelector('#'+burger.dataset.target);
 
-burger.classList.toggle('is-active');
-burgernav.classList.toggle('is-active');
-}
+  burger.classList.toggle('is-active');
+  burgernav.classList.toggle('is-active');
+};
 
+// interactive tap in the front page
 function openTab(event, tabId) {
-
   // Hid all the content of tabs
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i=0; i<tabcontent.length; i++) {
@@ -25,32 +32,13 @@ function openTab(event, tabId) {
   // show the content of tab that is-active
   document.getElementById(tabId).style.display = 'block';
   event.currentTarget.className += ' is-active';
-}
-
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  //loop through a collection of all HTML elements:
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("html-snippet");
-    if (file) {
-      //make an HTTP request using the attribute value as the file name:
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          //remove the attribute, and call this function once more:
-          elmnt.removeAttribute("html-snippet");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      //exit the function:
-      return;
-    }
-  }
 };
+
+// toggle dark and light mode
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+};
+
+
